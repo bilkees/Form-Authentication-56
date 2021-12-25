ec05ae3355beb5973315d33aeb526715d9abf57d
bilkees <88930592+bilkees@users.noreply.github.com>
2021-12-25 23:10:32 +0400

    Add project files.

---
+++ Form Authentication 56/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Form_Authentication_56.Models;
+using System.Web.Security;
+
+namespace Form_Authentication_56.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AuthorizeEntities2 _dbContext = new AuthorizeEntities2();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = _dbContext.Users
+               .Any(u => u.UserName.ToLower() == user
+               .UserName.ToLower() && user
+               .Password == user.Password);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, false);
+                    return RedirectToAction("Index", "Employee");
+                }
+            }
+            ModelState.AddModelError("", "invalid Username or Password");
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Add(registerUser);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Login");
+
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Form Authentication 56/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Form_Authentication_56.MvcApplication" Language="C#" %>
